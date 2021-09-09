13c8e03b4c3844cce99b95d1ca0ad307e3fa2921
laphisboy <laphisboy@gmail.com>
2021-09-09 23:54:17 +0900

    initial commit

---
+++ build/ros_yolo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cilab/catkin_ws/src/ros_yolo
+# Build directory: /home/cilab/catkin_ws/build/ros_yolo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
